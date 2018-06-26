7b8746fc11159b6901b519302747ea277d05d53e
Ricardo Ruiz <rfpimpao@gmail.com>
2018-06-26 15:54:50 +0100

    passed join_string test

--- ruby_functions.rb
+++ ruby_functions.rb
@@ -22,3 +22,10 @@ def length_of_string (test_string)
   test_string = "A string of length 21"
   return test_string.length
 end
+
+def join_string (string_1, string_2)
+  string_1 = "Mary had a little lamb, "
+  string_2 = "its fleece was white as snow"
+  joined_string = string_1 + string_2
+  return joined_string
+end
